dfc162cfdf7aa98995bb56cb69099ad3822a36c7
vyshnavi-2803 <24wh1a05y9@bvrithyderabad.edu.in>
2026-07-04 10:15:21 +0530

    intial commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,43 @@
-console.log("hello from js!");
-alert("Hello from jS");
+const users = [
+  {
+    "name" :"Jane Doe",
+    "gender" : "Female",
+    "image" : "./images/jane.png"
+  },
+  {
+    "name" :"John Doe",
+    "gender" : "Male",
+    "image" : "./images/john.png"
+  },
+]
+let curUserId = 0;
+function toggle(){
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+  // image
+  document.getElementById("img").src=users[curUserId].image;
+  // name
+  document.getElementById("card-name").innerText=users[curUserId].name;
+}
+
+console.log("Hello from JS");
+
+function getRandomUser(){
+    fetch("https://randomuser.me/api")
+    .then(function (data){
+        return data.json()
+    })
+     .then(function(parsedData){
+        console.log(parsedData);
+        let gender=parsedData.results[0].gender;
+        let first=parsedData.results[0].name.first;
+        let last=parsedData.results[0].name.last;
+        let name=first+" "+last;
+        let imageUrl=parsedData.results[0].picture.large;
+        document.getElementById("card-gender").src=imageUrl;
+        document.getElementById("card-name").innerText=name;
+        document.getElementById("img").src=imageUrl;
+     })
+}
